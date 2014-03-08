e2698fc342a78a67d0316d1f6fac8fe6b5b29d5e
Xannax Prozaxx <xananax@yelostudio.com>
2014-03-08 19:34:34 +0200

    added empty state for pledges

--- chunks/records.php
+++ chunks/records.php
@@ -1,26 +1,30 @@
-<table class="table">
-	<thead>
+<?php if($records):?>
+	<table class="table">
+		<thead>
+			<tr>
+				<td><?php echo l('name') ?></td>
+				<td><?php echo l('type') ?></td>
+			</tr>
+		</thead>
+		<tbody>
+	<?php foreach($records as $r): ?>
 		<tr>
-			<td><?php echo l('name') ?></td>
-			<td><?php echo l('type') ?></td>
+			<td>
+				<?php
+					if(isset($r['website']) && $r['website']){echo '<a href="'.$r['website'].'" target="_blank">';};
+					if(isset($r['name_first'])){echo $r['name_first'];};
+					if(isset($r['name_last'])){echo ' '.$r['name_last'];};
+					if(isset($r['company'])){echo $r['company'];};
+					if(isset($r['website']) && $r['website']){echo '</a>';};
+				?>
+			</td>
+			<td>
+				<i class="icon-<?php echo ($r['type']=='company' ? 'group':'user'); ?>"></i> <?php echo l($r['type']); ?>
+			</td>
 		</tr>
-	</thead>
-	<tbody>
-<?php foreach($records as $r): ?>
-	<tr>
-		<td>
-			<?php
-				if(isset($r['website']) && $r['website']){echo '<a href="'.$r['website'].'" target="_blank">';};
-				if(isset($r['name_first'])){echo $r['name_first'];};
-				if(isset($r['name_last'])){echo ' '.$r['name_last'];};
-				if(isset($r['company'])){echo $r['company'];};
-				if(isset($r['website']) && $r['website']){echo '</a>';};
-			?>
-		</td>
-		<td>
-			<i class="icon-<?php echo ($r['type']=='company' ? 'group':'user'); ?>"></i> <?php echo l($r['type']); ?>
-		</td>
-	</tr>
-<?php endforeach; ?>
-	</tbody>
-</table>
+	<?php endforeach; ?>
+		</tbody>
+	</table>
+<?php else:?>
+	<p><?php echo l('no_records') ?></p>
+<?php endif;?>
