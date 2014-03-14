5d696a5d88c3f80fa1edb91f54a4398c1dcde3a9
Xannax Prozaxx <xananax@yelostudio.com>
2014-03-14 11:42:03 +0200

    added arabic poster

--- chunks/body.php
+++ chunks/body.php
@@ -1,2 +1,5 @@
-<img src="poster.jpg" alt="Earth Hour" style="width:100%;height:auto">
+<span class="languages">
+	<img src="poster-en.jpg" alt="Earth Hour" style="width:100%;height:auto" class="language-en" dir="rtl">
+	<img src="poster-ar.jpg" alt="Earth Hour" style="width:100%;height:auto" class="language-ar" dir="rtl">
+</span>
 <img src="SPONSORS.jpg" alt="Earth Hour Sponsors" style="width:100%;height:auto">
